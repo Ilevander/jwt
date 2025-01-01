2012eda7fb73a0d29a5e4566bd0e54da64a56cbc
dell <dell@DESKTOP-4P8U8AR>
2025-01-01 16:20:38 +0100

    Generating token (console visibility)

---
+++ src/main/java/com/ilyass/tests/Test1.java
@@ -0,0 +1,18 @@
+package com.ilyass.tests;
+
+import java.util.Date;
+
+import com.ilyass.jwt.TokenManager;
+
+public class Test1 {
+
+	private final static long PERIOD_VALIDITY = 1 * 24 * 60 * 60 * 1000;
+	public static void main(String[] args) {
+		Date dateCreation = new Date();
+		Date dateExpiration = new Date(dateCreation.getTime() + PERIOD_VALIDITY);
+		String token=TokenManager.generateToken("admin", dateCreation, dateExpiration);
+		System.out.println(token);
+		
+	}
+
+}
